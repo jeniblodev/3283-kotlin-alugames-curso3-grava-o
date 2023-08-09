7da3c98f0f436d9885f1781178304bb37408f4b9
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-08-09 16:01:22 -0300

    3.3

--- src/main/kotlin/br/com/alura/alugames/dados/DAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/DAO.kt
@@ -1,12 +1,17 @@
 package br.com.alura.alugames.dados
 
+import br.com.alura.alugames.modelo.Jogo
 import javax.persistence.EntityManager
 
-abstract class DAO <TModel>(protected val manager: EntityManager) {
+abstract class DAO <TModel, TEntity>(protected val manager: EntityManager, protected val entityType: Class<TEntity>) {
 
-    abstract fun toEntity(objeto: TModel)
+    abstract fun toEntity(objeto: TModel): TEntity
+    abstract fun toModel(entity: TEntity): TModel
 
-    abstract fun getLista(): List<TModel>
+    open fun getLista(): List<TModel> {
+        val query = manager.createQuery("FROM ${entityType.simpleName}", entityType)
+        return query.resultList.map { entity -> toModel(entity) }
+    }
     open fun adicionar(objeto: TModel) {
         val entity = toEntity(objeto)
         manager.transaction.begin()
