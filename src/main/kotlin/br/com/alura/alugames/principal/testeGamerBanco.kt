47da32ca217b66b630a0587350f3372050fb2b1b
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-08-10 10:38:18 -0300

    projeto inicial 4.3

---
+++ src/main/kotlin/br/com/alura/alugames/principal/testeGamerBanco.kt
@@ -0,0 +1,20 @@
+package br.com.alura.alugames.principal
+
+import br.com.alura.alugames.dados.Banco
+import br.com.alura.alugames.dados.GamersDAO
+import br.com.alura.alugames.modelo.Gamer
+
+fun main(){
+    val gamer = Gamer("Monica", "monica@email.com", "15/03/1995", "moni")
+
+    val manager = Banco.getEntityManager()
+    val gamerDAO = GamersDAO(manager)
+
+
+    gamerDAO.adicionar(gamer)
+
+    val listaGamersBanco = gamerDAO.getLista()
+    println(listaGamersBanco)
+
+    manager.close()
+}
